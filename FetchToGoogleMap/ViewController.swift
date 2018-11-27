4539fef090f5639fb60243723ca0d02a1ff042cd
Preuttipan <terrorize.nut@gmail.com>
2018-11-27 11:59:51 +0700

    Initial Commit

---
+++ FetchToGoogleMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FetchToGoogleMap
+//
+//  Created by Preuttipan Janpen on 27/11/2561 BE.
+//  Copyright © 2561 Lphant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
